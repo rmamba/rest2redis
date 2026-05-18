c1244f0a9346b609e08fa474a15bedc58dd1afbc
rmamba <rmamba@gmail.com>
2026-05-18 16:36:54 +0700

    Fix server types

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ const run = async () => {
 
     console.log(`Setting up express server on port ${WEBUI_PORT}...`);
     const app = express();
-    app.use(express.json());
+    app.use(express.text({ type: 'text/plain' }));
+    app.use(express.json({ type: 'text/json' }));
     app.use(cors());
 
     app.get('/', function (req, res) {
@@ -89,7 +90,9 @@ const run = async () => {
             topic: url,
             type: cmd,
             size: bodySize,
+            // data: req.body,
         };
+
         switch (cmd) {
             case 'publish':
                 redisClient.publish(topic, req.body);
